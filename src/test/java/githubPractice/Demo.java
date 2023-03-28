c947854ad7cc61ec46255ea46ce8026380c2928d
USER <USER@DESKTOP-PMHUQ0L>
2023-03-28 20:27:22 +0530

    Demo script committed

---
+++ src/test/java/githubPractice/Demo.java
@@ -0,0 +1,17 @@
+package githubPractice;
+
+import org.testng.annotations.Test;
+
+public class Demo {
+	
+		@Test
+		public void sample() {
+		
+		System.out.println("Sample");
+		System.out.println("Sample2");
+		System.out.println("Sample1");
+		
+			
+
+}
+}
